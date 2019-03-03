e3081506cd629370843b6aebb3be9d38d737800d
belgort-clark <belgort@clark.edu>
2019-03-03 11:18:47 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ctec-121-lab-8
+# CTEC 121 - Lab No. 8
+
+## Overview
+
+Using the Python script [ctec-121-lab-8.py](ctec-121-lab-8.py) and the [welcome.html](welcome.html) file provided, run and study the code. This lab will show you how you can use Python read HTML files.
+
+Wherever you see **COMMENT THIS**, you are to comment the code to the best of your ability. 
+
+You may have to use the documentation on the [Python.org](http://python.org) site. 
+
+## Submission
+
+Stage, commit and push your code back to GitHub. 
---
+++ ctec-121-lab-8.py
@@ -0,0 +1,55 @@
+# CTEC 121
+# Parsing HTML files using Python
+# CTEC 121 - Clark College
+# Reading HTML files with Python
+# by Bruce Elgort
+# August 20, 2017
+# Lab No. 8
+
+# HTML Parser library - https://docs.python.org/3/library/html.parser.html
+from html.parser import HTMLParser
+
+# io library - https://docs.python.org/3/library/io.html
+import io
+
+class MyHTMLParser(HTMLParser):
+    # COMMENT THIS
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    # COMMENT THIS
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    # COMMENT THIS
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+    # COMMENT THIS
+    def handle_comment(self,data):
+        print("Encountered a comment  :",data)
+
+    # COMMENT THIS
+    def unknown_decl(self,data):
+        print("Encountered something unknown!",data)
+
+def main():
+    print("Starting...")
+    
+    # COMMENT THIS
+    parser = MyHTMLParser()
+
+    # COMMENT THIS
+    filename = 'welcome_page.html'
+
+    # COMMENT THIS
+    with io.open(filename,'r',encoding='utf8') as f:
+        # COMMENT THIS
+        text = f.read()
+        # COMMENT THIS
+        parser.feed(text)
+
+    print("Parsing completed!")
+
+    
+main()    
